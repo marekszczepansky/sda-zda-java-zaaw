8b26bd754521b33964fce3ffe3074cbb8081923a
Marek Szczepanski <mszczepanski2@atlassian.com>
2020-10-18 12:31:33 +0200

    task9 template: Circle, Point2D

---
+++ src/main/java/sda/szczepanski/java/zaaw/task9/Circle.java
@@ -0,0 +1,28 @@
+package sda.szczepanski.java.zaaw.task9;
+
+import java.util.List;
+
+public class Circle {
+    public Circle(Point2D center, Point2D point) {
+
+    }
+
+    //    pierwiastek(x*x + y*y)
+    public double getRadius() {
+        return 0.0;
+    }
+
+    //    2*pi*r
+    public double getPerimeter() {
+        return 0.0;
+    }
+
+    //    pi*r*r
+    public double getArea() {
+        return 0.0;
+    }
+
+    List<Point2D> getSlicePoints() {
+        return null;
+    }
+}
---
+++ src/main/java/sda/szczepanski/java/zaaw/task9/Point2D.java
@@ -0,0 +1,4 @@
+package sda.szczepanski.java.zaaw.task9;
+
+public class Point2D {
+}
