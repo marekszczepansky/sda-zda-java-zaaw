25bd1b8e9cad4090ff3eb4b6cedc4ec5f2cb44f8
Marek Szczepanski <mszczepanski2@atlassian.com>
2020-10-18 11:37:33 +0200

    Task 7, Cartridge solution

--- src/main/java/sda/szczepanski/java/zaaw/Cartridge.java
+++ src/main/java/sda/szczepanski/java/zaaw/Cartridge.java
@@ -1,19 +1,27 @@
 package sda.szczepanski.java.zaaw;
 
-public class Cartridge {
-    public Cartridge(int cartridgeSize) {
+import java.util.Stack;
 
+public class Cartridge {
+    private final int cartridgeSize;
+    private final Stack<String> content;
+
+    public Cartridge(int cartridgeSize) {
+        this.cartridgeSize = cartridgeSize;
+        content = new Stack<>();
     }
 
     void loadBullet(String bullet) {
+        if (content.size() >= cartridgeSize)
+            throw new CartridgeSizeExceeded();
+        content.push(bullet);
     }
 
     Boolean isLoaded() {
-        return null;
+        return !content.empty();
     }
 
     String shot() {
-        return null;
+        return content.empty() ? "pusty magazynek" : content.pop();
     }
-
 }
