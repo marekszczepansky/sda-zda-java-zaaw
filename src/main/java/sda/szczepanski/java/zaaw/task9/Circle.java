c0c7012f805833993505ed7bf09d2c3014ac1d3b
Marek Szczepanski <mszczepanski2@atlassian.com>
2020-10-18 13:01:03 +0200

    task9 Circle implemented

--- src/main/java/sda/szczepanski/java/zaaw/task9/Circle.java
+++ src/main/java/sda/szczepanski/java/zaaw/task9/Circle.java
@@ -3,23 +3,32 @@ package sda.szczepanski.java.zaaw.task9;
 import java.util.List;
 
 public class Circle {
-    public Circle(Point2D center, Point2D point) {
+    private final Point2D center;
+    private final Point2D point;
 
+    public Circle(Point2D center, Point2D point) {
+        this.center = center;
+
+        this.point = point;
     }
 
     //    pierwiastek(x*x + y*y)
     public double getRadius() {
-        return 0.0;
+        double xDistance = Math.abs(point.getX() - center.getX());
+        double yDistance = Math.abs(point.getY()) - center.getY();
+
+        return Math.sqrt(xDistance * xDistance + yDistance * yDistance);
     }
 
     //    2*pi*r
     public double getPerimeter() {
-        return 0.0;
+        return 2.0 * Math.PI * getRadius();
     }
 
     //    pi*r*r
     public double getArea() {
-        return 0.0;
+        final double radius = getRadius();
+        return Math.PI * radius * radius;
     }
 
     List<Point2D> getSlicePoints() {
