5fbbee2c24c99d3ef45ec83367ed782d6c2abe9b
Marek Szczepanski <mszczepanski2@atlassian.com>
2020-10-18 12:37:20 +0200

    task9 template: CircleTest

---
+++ src/test/java/sda/szczepanski/java/zaaw/task9/CircleTest.java
@@ -0,0 +1,38 @@
+package sda.szczepanski.java.zaaw.task9;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircleTest {
+
+    private static final Point2D TEST_CENTER = null;
+    private static final Point2D TEST_POINT = null;
+    private Circle circle;
+
+    @BeforeEach
+    void setUp() {
+        circle = new Circle(TEST_CENTER, TEST_POINT);
+    }
+
+    @Test
+    void shouldCalculateRadius() {
+
+    }
+
+    @Test
+    void shouldCalculatePerimeter() {
+
+    }
+
+    @Test
+    void shouldCalculateArea() {
+
+    }
+
+    @Test
+    void shouldCalculateSlicePoints() {
+
+    }
+}
