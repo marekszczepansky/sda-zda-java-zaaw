43d76a7d2f673bb88450e5c47617002bce505392
Marek Szczepanski <mszczepanski2@atlassian.com>
2020-10-18 10:47:50 +0200

    Task 7 - unit test

--- src/main/java/sda/szczepanski/java/zaaw/Cartridge.java
+++ src/main/java/sda/szczepanski/java/zaaw/Cartridge.java
@@ -1,11 +1,15 @@
 package sda.szczepanski.java.zaaw;
 
 public class Cartridge {
+    public Cartridge(int cartridgeSize) {
+
+    }
+
     void loadBullet(String bullet) {
     }
 
-    boolean isLoaded() {
-        return false;
+    Boolean isLoaded() {
+        return null;
     }
 
     String shot() {
--- src/test/java/sda/szczepanski/java/zaaw/CartridgeTest.java
+++ src/test/java/sda/szczepanski/java/zaaw/CartridgeTest.java
@@ -1,12 +1,50 @@
 package sda.szczepanski.java.zaaw;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CartridgeTest {
 
+    private static String[] TEST_LOAD = new String[]{"Bardzo", "szybkostrzelny", "karabin", "testowy"};
+    private static int cartridgeSize = TEST_LOAD.length;
+    private Cartridge emptyCartridge;
+    private Cartridge loadedCartridge;
+
     @BeforeEach
     void setUp() {
+        emptyCartridge = new Cartridge(cartridgeSize);
+        loadedCartridge = new Cartridge(cartridgeSize);
+        for (String bullet : TEST_LOAD) {
+            loadedCartridge.loadBullet(bullet);
+        }
+    }
+
+    @Test
+    void shouldCheckIfLoadedWhenEmpty() {
+        assertFalse(emptyCartridge.isLoaded());
+    }
+
+    @Test
+    void shouldCheckIfLoadedWhenNotEmpty() {
+        assertTrue(loadedCartridge.isLoaded());
+    }
+
+    @Test
+    void shouldShotWhenEmpty() {
+        assertEquals("pusty magazynek", emptyCartridge.shot());
+    }
+
+    @Test
+    void shouldShotWhenLoaded() {
+        assertEquals(TEST_LOAD[TEST_LOAD.length-1], loadedCartridge.shot());
+    }
+
+    @Test
+    void shouldShotManyWhenLoaded() {
+        loadedCartridge.shot();
+        loadedCartridge.shot();
+        assertEquals(TEST_LOAD[TEST_LOAD.length-3], loadedCartridge.shot());
     }
 }
