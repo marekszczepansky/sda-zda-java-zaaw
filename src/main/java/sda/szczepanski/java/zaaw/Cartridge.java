b4d388214c62e1b3f7a77574db9d0c9c93db0a50
Marek Szczepanski <mszczepanski2@atlassian.com>
2020-10-18 10:21:23 +0200

    Task 7 - gun cartridge

---
+++ src/main/java/sda/szczepanski/java/zaaw/Cartridge.java
@@ -0,0 +1,15 @@
+package sda.szczepanski.java.zaaw;
+
+public class Cartridge {
+    void loadBullet(String bullet) {
+    }
+
+    boolean isLoaded() {
+        return false;
+    }
+
+    String shot() {
+        return null;
+    }
+
+}
