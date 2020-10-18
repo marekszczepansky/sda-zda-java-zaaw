605d18f52b03a4efadf1bf4c30f79f07f5ecdd41
Marek Szczepanski <mszczepanski2@atlassian.com>
2020-10-18 08:58:09 +0200

    WItam ZDJAVApol33

--- src/main/java/sda/szczepanski/java/zaaw/MainProgram.java
+++ src/main/java/sda/szczepanski/java/zaaw/MainProgram.java
@@ -2,5 +2,7 @@ package sda.szczepanski.java.zaaw;
 
 public class MainProgram {
     public static void main(String[] args) {
+        System.out.println("Witam na zajęciach java zaawansowana - programowanie");
+        System.out.println("Grupa ZDJAVApol33");
     }
 }
