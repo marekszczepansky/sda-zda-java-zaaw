74de34b13c05d5f015a6557de4f72bf5845f221f
Marek Szczepanski <mszczepanski2@atlassian.com>
2020-10-24 08:13:03 +0200

    SmallTasks updated

--- src/main/java/sda/szczepanski/java/zaaw/task1and2/SmallTasks.java
+++ src/main/java/sda/szczepanski/java/zaaw/task1and2/SmallTasks.java
@@ -1,11 +1,13 @@
 package sda.szczepanski.java.zaaw.task1and2;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SmallTasks {
-    public static List<String> alphabeticSort(List<String> inputList){
 
-        return null;
+    public static List<String> alphabetZtoASortedList(List<String> list){
+        return list.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
     }
 
     public static List<String> alphabetZtoACISortedList(List<String> list){
