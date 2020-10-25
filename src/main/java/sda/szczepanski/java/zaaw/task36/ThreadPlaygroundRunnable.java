30546b9ea7821734886f02df93ea4640f0f431cb
Marek Szczepanski <mszczepanski2@atlassian.com>
2020-10-25 11:07:05 +0100

    Task36 new line printed

--- src/main/java/sda/szczepanski/java/zaaw/task36/ThreadPlaygroundRunnable.java
+++ src/main/java/sda/szczepanski/java/zaaw/task36/ThreadPlaygroundRunnable.java
@@ -10,7 +10,7 @@ public class ThreadPlaygroundRunnable implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
-            System.out.printf("Thread: %s, name: %s, iteration: %d",
+            System.out.printf("Thread: %s, name: %s, iteration: %d\n",
                     Thread.currentThread().getName(), name, i);
         }
     }
