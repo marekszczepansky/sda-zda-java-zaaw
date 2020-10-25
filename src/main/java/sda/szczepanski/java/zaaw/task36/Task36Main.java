0a37f9353bae1e6299bc22a4733f59e867b20436
Marek Szczepanski <mszczepanski2@atlassian.com>
2020-10-25 10:55:40 +0100

    Task36 introduced

---
+++ src/main/java/sda/szczepanski/java/zaaw/task36/Task36Main.java
@@ -0,0 +1,10 @@
+package sda.szczepanski.java.zaaw.task36;
+
+public class Task36Main {
+    private static Thread thread1;
+    private static Thread thread2;
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/sda/szczepanski/java/zaaw/task36/ThreadPlaygroundRunnable.java
@@ -0,0 +1,8 @@
+package sda.szczepanski.java.zaaw.task36;
+
+public class ThreadPlaygroundRunnable implements Runnable {
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+}
