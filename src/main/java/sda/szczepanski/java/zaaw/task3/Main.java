deb2fa19d6ba2fb586ec2e59e199d554293e4b1b
Marek Szczepanski <mszczepanski2@atlassian.com>
2021-02-13 10:43:03 +0100

    Task 3 completed

--- src/main/java/sda/szczepanski/java/zaaw/task3/Main.java
+++ src/main/java/sda/szczepanski/java/zaaw/task3/Main.java
@@ -15,9 +15,22 @@ public class Main {
         language.put("C#", 4);
 
         final Set<String> keySet = language.keySet();
+        int toGo = keySet.size();
         for (String key : keySet) {
-            System.out.println("Klucz: " + key + ", Wartość: " + language.get(key));
+            final String separator = --toGo == 0 ? "." : ",";
+            System.out.println("Klucz: " + key + ", Wartość: " + language.get(key) + separator);
         }
+
+        System.out.println("---");
+        final Set<Map.Entry<String, Integer>> entries = language.entrySet();
+        entries.forEach(entry ->{
+            System.out.println("Klucz: " + entry.getKey() + ", Wartość: " + entry.getValue());
+        });
+
+        System.out.println("---");
+        language.forEach((key, value) -> {
+            System.out.println("Klucz: " + key + ", Wartość: " + value);
+        });
 
 
     }
